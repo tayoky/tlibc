88efe542980d99922c7cf744ae288943f5fe92fb
tayoky <tayoky4848@gmail.com>
2025-02-10 08:14:37 +0100

    add stddef

---
+++ include/stddef.h
@@ -0,0 +1,18 @@
+#ifndef STDDEF_H
+#define STDDEF_H
+
+
+
+#ifndef NULL
+#define NULL
+#endif
+
+typedef unsigned long size_t;
+
+typedef signed long ptrdiff_t;
+
+typedef char wchar_t;
+
+//TODO : offsetof() macro
+
+#endif

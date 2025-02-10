d9992f9ec422fcabdc49f6960b641ac5e558d9ba
tayoky <tayoky4848@gmail.com>
2025-02-10 19:34:53 +0100

    better install script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cp ./crt0.o $1
-cp ./tlibc.a $1/libc.a
+mkdir -p $1/include
+mkdir -p $1/lib
+cp ./crt0.o $1/lib/crt0.o
+cp ./tlibc.a $1/lib/libc.a
+cp $(find ./include/ -name "*.h") $1/include

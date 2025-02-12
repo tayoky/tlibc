e392061ecd39a2b7d6dc691594c5cfd8d49204db
tayoky <tayoky4848@gmail.com>
2025-02-12 19:14:07 +0100

    add stdarg and mode support for open

--- src/stanix/fcntl.c
+++ src/stanix/fcntl.c
@@ -1,9 +1,18 @@
 #include <fcntl.h>
 #include <syscall.h>
+#include <stdarg.h>
 
 
 int open(const char *pathname, int flags, ... /* mode_t mode */ ){
-	return __syscall3(SYS_OPEN,(long)pathname,flags,0);
+	//get the mode if open
+	mode_t mode = 0;
+	if(flags & O_CREAT){
+		va_list args;
+		va_start(args,flags);
+		mode = va_arg(args,mode_t);
+		va_end(args);
+	}
+	return __syscall3(SYS_OPEN,(long)pathname,flags,mode);
 }
 
 int creat(const char *pathname, mode_t mode){

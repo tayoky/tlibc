c59bd009cc02d60743704218de18bc5ab5001db4
tayoky <tayoky4848@gmail.com>
2025-02-20 20:51:47 +0000

    add some limits

---
+++ include/inttypes.h
@@ -0,0 +1,8 @@
+#ifndef INTTYPES_H
+#define INTTYPES_H
+
+//just include stdint.h LOL
+
+#include <stdint.h>
+
+#endif
--- include/stdint.h
+++ include/stdint.h
@@ -54,4 +54,23 @@ typedef int64_t  intptr_t;
 
 typedef uint64_t uintptr_t;
 
+#ifndef INT_MAX
+#define INT_MAX 2147483647
+#endif
+#ifndef LONG_MAX
+#define LONG_MAX 9223372036854775807
+#endif
+#ifndef INT_MIN
+#define INT_MIN -2147483648
+#endif
+#ifndef LONG_MIN
+#define LONG_MIN -9223372036854775808
+#endif
+#ifndef UINT_MAX
+#define UINT_MAX 0xFFFFFFFF
+#endif
+#ifndef ULONG_MAX
+#define ULONG_MAX 0xFFFFFFFFFFFFFFFF
+#endif
+
 #endif

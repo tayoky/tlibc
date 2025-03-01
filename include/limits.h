c29f73fa016d22ab6ae955aa43675affaf644ae7
tayoky <tayoky4848@gmail.com>
2025-03-01 19:42:59 +0100

    more integer limits

--- include/limits.h
+++ include/limits.h
@@ -1,23 +1,7 @@
 #ifndef LIMITS_H
 #define LIMITS_H
 
-#ifndef INT_MAX
-#define INT_MAX 2147483647
-#endif
-#ifndef LONG_MAX
-#define LONG_MAX 9223372036854775807
-#endif
-#ifndef INT_MIN
-#define INT_MIN -2147483648
-#endif
-#ifndef LONG_MIN
-#define LONG_MIN -9223372036854775808
-#endif
-#ifndef UINT_MAX
-#define UINT_MAX 0xFFFFFFFF
-#endif
-#ifndef ULONG_MAX
-#define ULONG_MAX 0xFFFFFFFFFFFFFFFF
-#endif
+//lazy way of getting all integer related limits
+#include <stdint.h>
 
 #endif
--- include/stdint.h
+++ include/stdint.h
@@ -54,23 +54,22 @@ typedef int64_t  intptr_t;
 
 typedef uint64_t uintptr_t;
 
-#ifndef INT_MAX
-#define INT_MAX 2147483647
-#endif
-#ifndef LONG_MAX
-#define LONG_MAX 9223372036854775807
-#endif
-#ifndef INT_MIN
+#define CHAR_BIT 8
+
+#define SCHAR_MIN -128
+#define SCHAR_MAX 127
+#define UCHAR_MAX 0xFF
+
 #define INT_MIN -2147483648
-#endif
-#ifndef LONG_MIN
-#define LONG_MIN -9223372036854775808
-#endif
-#ifndef UINT_MAX
+#define INT_MAX 2147483647
 #define UINT_MAX 0xFFFFFFFF
-#endif
-#ifndef ULONG_MAX
+
+#define SHRT_MIN -32768
+#define SHRT_MAX 32767
+#define USHRT_MAX 0xFFFF
+
+#define LONG_MIN -9223372036854775808
+#define LONG_MAX 9223372036854775807
 #define ULONG_MAX 0xFFFFFFFFFFFFFFFF
-#endif
 
 #endif

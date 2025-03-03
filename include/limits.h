486f82210825143dcbfb2dadde9746deb04ab5f4
tayoky <tayoky4848@gmail.com>
2025-03-03 18:21:38 +0100

    move integers limits to the good place

--- include/limits.h
+++ include/limits.h
@@ -1,7 +1,25 @@
 #ifndef LIMITS_H
 #define LIMITS_H
 
-//lazy way of getting all integer related limits
-#include <stdint.h>
+
+//intergers limits
+
+#define CHAR_BIT 8
+
+#define SCHAR_MIN -128
+#define SCHAR_MAX 127
+#define UCHAR_MAX 0xFF
+
+#define INT_MIN -2147483648
+#define INT_MAX 2147483647
+#define UINT_MAX 0xFFFFFFFF
+
+#define SHRT_MIN -32768
+#define SHRT_MAX 32767
+#define USHRT_MAX 0xFFFF
+
+#define LONG_MIN -9223372036854775808
+#define LONG_MAX 9223372036854775807
+#define ULONG_MAX 0xFFFFFFFFFFFFFFFF
 
 #endif

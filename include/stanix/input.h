2d105f83fc06f6e594b9376b0711a6dec9d81db3
tayoky <tayoky4848@gmail.com>
2025-03-08 18:11:31 +0100

    new input.h

--- include/stanix/input.h
+++ include/stanix/input.h
@@ -1,9 +1,75 @@
-#ifndef INPU_H
+#ifndef INPUT_H
 #define INPUT_H
 
-#define KEY_ESCAPE   0x1B
-#define KEY_SHIFT    0x0F
-#define KEY_CAPSLOCK 0x11
+#include <sys/time.h>
 
+struct input_event {
+struct timeval timestamp;
+unsigned long ie_class;
+unsigned long ie_subclass;
+unsigned long ie_type;
+union {
+struct {
+unsigned long axis;
+long x;
+long y;
+} ie_move;
+struct {
+char c;
+char padding[3];
+unsigned long scancode;
+unsigned long flags;
+}
+char ie_string[24];
+}
+}
+
+struct input_request {
+unsigned long ir_type;
+unsigned long ir_arg;
+}
+
+//non device specific
+#define IR_INFO_REQUEST        0x01
+#define IR_VENDOR_REQUEST      0x02
+#define IR_NAME_REQUEST        0x03
+#define IR_RESET_REQUEST       0x04
+
+//device specific
+#define IR_SET_LED_REQUEST     0x05
+#define IR_SET_LAYOUT_REQUEST  0x06
+
+//event types
+#define IE_UNKOW_EVENT  0x00
+#define IE_ACK_EVENT    0x01
+#define IE_MOVE_EVENT   0x02
+#define IE_KEY_EVENT    0x03
+
+//class
+#define IE_CLASS_UNKNOW    0x00
+#define IE_CLASS_KEYBOARD  0x01
+#define IE_CLASS_MOUSE     0x02
+#define IE_CLASS_JOYSTICK  0x03
+
+
+//subclass
+#define IE_SUBCLASS_UNKNOW        0x00
+
+//keyboard subclass
+#define IE_SUBCLASS_USB_KBD       0x01
+#define IE_SUBCLASS_PS2_KBD       0x02
+#define IE_SUBCALSS_VIRT_KBD      0x03
+
+//mouse subclass
+#define IE_SUBCLASS_USB_MOUSE     0x01
+#define IE_SUBCLASS_PS2_MOUSE     0x02
+#define IE_SUBCLASS_VIRT_MOUSE    0x03
+#define IE_SUBCLASS_PS2_TRACKPAD  0x04
+
+//key event flags
+#define IE_KEY_PRESS    0x01
+#define IE_KEY_RELEASE  0x02
+#define IE_KEY_HOLD     0x03
+#define IE_KEY_GRPAH    0x04 //printable key
 
 #endif

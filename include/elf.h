4a13d727345d001fd545c1da375be39c9f6357e6
tayoky <tayoky4848@gmail.com>
2025-04-13 19:36:39 +0200

    fix SHT_XXX in elf.h

--- include/elf.h
+++ include/elf.h
@@ -144,22 +144,22 @@ typedef struct{
 } Elf64_Shdr;
 
 //sh_type / section types
-#define SHT_NULL     //0
-#define SHT_PROGBITS //1
-#define SHT_SYMTAB   //2
-#define SHT_STRTAB   //3
-#define SHT_RELA     //4
-#define SHT_HASH     //5
-#define SHT_DYNAMIC  //6
-#define SHT_NOTE     //7
-#define SHT_NOBITS   //8
-#define SHT_REL      //9
-#define SHT_SHLIB    //10
-#define SHT_DYNSYM   //11
-#define SHT_LOPROC   //0x70000000
-#define SHT_HIPROC   //0x7fffffff
-#define SHT_LOUSER   //0x80000000
-#define SHT_HIUSER   //0xffffffff
+#define SHT_NULL     0
+#define SHT_PROGBITS 1
+#define SHT_SYMTAB   2
+#define SHT_STRTAB   3
+#define SHT_RELA     4
+#define SHT_HASH     5
+#define SHT_DYNAMIC  6
+#define SHT_NOTE     7
+#define SHT_NOBITS   8
+#define SHT_REL      9
+#define SHT_SHLIB    10
+#define SHT_DYNSYM   11
+#define SHT_LOPROC   0x70000000
+#define SHT_HIPROC   0x7fffffff
+#define SHT_LOUSER   0x80000000
+#define SHT_HIUSER   0xffffffff
 
 //symbols
 

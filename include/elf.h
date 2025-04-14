6ea167b56d8ed953d3599ca0fa6ffc1760607f87
tayoky <tayoky4848@gmail.com>
2025-04-14 19:10:07 +0200

    elf.h : add relocation types

--- include/elf.h
+++ include/elf.h
@@ -247,6 +247,40 @@ typedef struct {
 #define ELF64_R_TYPE(i) ((i) & 0xffffffffL)
 #define ELF64_R_INFO(s, t) (((s) << 32) + ((t) & 0xffffffffL))
 
+//relocations types
+
+//x86_64
+#define R_X86_64_NONE           0       /* No reloc */
+#define R_X86_64_64             1       /* Direct 64 bit  */
+#define R_X86_64_PC32           2       /* PC relative 32 bit signed */
+#define R_X86_64_GOT32          3       /* 32 bit GOT entry */
+#define R_X86_64_PLT32          4       /* 32 bit PLT address */
+#define R_X86_64_COPY           5       /* Copy symbol at runtime */
+#define R_X86_64_GLOB_DAT       6       /* Create GOT entry */
+#define R_X86_64_JUMP_SLOT      7       /* Create PLT entry */
+#define R_X86_64_RELATIVE       8       /* Adjust by program base */
+#define R_X86_64_GOTPCREL       9       /* 32 bit signed PC relative offset to GOT */
+#define R_X86_64_32             10      /* Direct 32 bit zero extended */
+#define R_X86_64_32S            11      /* Direct 32 bit sign extended */
+#define R_X86_64_16             12      /* Direct 16 bit zero extended */
+#define R_X86_64_PC16           13      /* 16 bit sign extended pc relative */
+#define R_X86_64_8              14      /* Direct 8 bit sign extended  */
+#define R_X86_64_PC8            15      /* 8 bit sign extended pc relative */
+#define R_X86_64_DTPMOD64       16      /* ID of module containing symbol */
+#define R_X86_64_DTPOFF64       17      /* Offset in module's TLS block */
+#define R_X86_64_TPOFF64        18      /* Offset in initial TLS block */
+#define R_X86_64_TLSGD          19      /* 32 bit signed PC relative offset to two GOT entries for GD symbol */
+#define R_X86_64_TLSLD          20      /* 32 bit signed PC relative offset to two GOT entries for LD symbol */
+#define R_X86_64_DTPOFF32       21      /* Offset in TLS block */
+#define R_X86_64_GOTTPOFF       22      /* 32 bit signed PC relative offset to GOT entry for IE symbol */
+#define R_X86_64_TPOFF32        23      /* Offset in initial TLS block */
+#define R_X86_64_PC64           24      /* PC relative 64 bit */
+#define R_X86_64_GOTOFF64       25      /* 64 bit offset to GOT */
+#define R_X86_64_GOTPC32        26      /* 32 bit signed pc relative offset to GOT */
+#define R_X86_64_GOT64          27      /* 64-bit GOT entry offset */
+#define R_X86_64_GOTPCREL64     28      /* 64-bit PC relative offset to GOT entry */
+#define R_X86_64_GOTPC64        29      /* 64-bit PC relative offset to GOT */
+
 
 //program header
 

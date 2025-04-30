b6b5b6e042e01ab2a1e1f19d2aa91e613f107ceb
tayoky <tayoky4848@gmail.com>
2025-04-30 20:16:19 +0200

    sys/types.h : adapt types to arch

--- include/stanix/sys/types.h
+++ include/stanix/sys/types.h
@@ -1,26 +1,36 @@
 #ifndef SYS_TYPES_H
 #define SYS_TYPES_H
 
-typedef long pid_t;
+#ifdef i386
+#define SYS_T int
+#else
+#define SYS_T long
+#endif
 
-typedef long uid_t;
-typedef long gid_t;
+typedef SYS_T pid_t;
+
+typedef SYS_T uid_t;
+typedef SYS_T gid_t;
 typedef int mode_t;
-typedef long nlink_t ;
+typedef SYS_T nlink_t ;
 
-typedef long time_t;
-typedef unsigned long useconds_t;
-typedef long suseconds_t;
+typedef SYS_T time_t;
+typedef unsigned SYS_T useconds_t;
+typedef SYS_T suseconds_t;
 
 //stupid bloat for posix API
-typedef long dev_t;
-typedef long ino_t;
-typedef long blksize_t; //WTF don't exist and why a program will need that ???
-typedef long blkcnt_t;
+typedef SYS_T dev_t;
+typedef SYS_T ino_t;
+typedef SYS_T blksize_t; //WTF don't exist and why a program will need that ???
+typedef SYS_T blkcnt_t;
 
-
+#ifdef i386
+typedef unsigned int off_t;
+typedef unsigned int size_t;
+typedef signed   int ssize_t;
+#else
 typedef unsigned long long off_t;
 typedef unsigned long long size_t;
 typedef signed   long long ssize_t;
-
+#endif
 #endif

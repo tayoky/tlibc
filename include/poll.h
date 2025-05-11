bd4a672d970885a1ba5525c068bb7e96f92b8577
tayoky <tayoky4848@gmail.com>
2025-05-11 19:01:07 +0200

    better poll.h withoot warnings

--- include/poll.h
+++ include/poll.h
@@ -12,15 +12,15 @@ typedef unsigned int nfds_t;
 int poll(struct pollfd *fds, nfds_t nfds, int timeout);
 
 
-#define POLLWRNORM 0x01                    //data on priority band 0 may be written
-#define POLLWRBAND 0x02                    //data on priority bands greater than 0 may be written
-#define POLLRDNORM 0x04                    //data on priority band 0 may be read
-#define POLLRDBAND 0x08                    //data on priority bands greater than 0 may be read
-#define POLLIN     POLLRDNORM | POLLRDBAND //same effect as POLLRDNORM | POLLRDBAND
-#define POLLPRI    0x10                    //high priority data may be read
-#define POLLOUT    POLLWRNORM              //same value as POLLWRNORM
-#define POLLERR    0x100                   //an error has occurred (revents only)
-#define POLLHUP    0x200                   //device has been disconnected (revents only)
-#define POLLNVAL   0x400                   //invalid fd member (revents only)
+#define POLLWRNORM 0x01                      //data on priority band 0 may be written
+#define POLLWRBAND 0x02                      //data on priority bands greater than 0 may be written
+#define POLLRDNORM 0x04                      //data on priority band 0 may be read
+#define POLLRDBAND 0x08                      //data on priority bands greater than 0 may be read
+#define POLLIN     (POLLRDNORM | POLLRDBAND) //same effect as POLLRDNORM | POLLRDBAND
+#define POLLPRI    0x10                      //high priority data may be read
+#define POLLOUT    POLLWRNORM                //same value as POLLWRNORM
+#define POLLERR    0x100                     //an error has occurred (revents only)
+#define POLLHUP    0x200                     //device has been disconnected (revents only)
+#define POLLNVAL   0x400                     //invalid fd member (revents only)
 
 #endif

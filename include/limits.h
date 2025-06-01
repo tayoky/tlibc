59fdfec18b8fdcf8589810d7aca8659add2406e7
tayoky <tayoky4848@gmail.com>
2025-06-01 16:49:13 +0200

    limits.h : get MAX from compiler

--- include/limits.h
+++ include/limits.h
@@ -4,23 +4,58 @@
 
 //intergers limits
 
+#ifdef __CHAR_BIT__
+#define CHAR_BIT __CHAR_BIT__
+#else
 #define CHAR_BIT 8
+#endif
 
-#define SCHAR_MIN -128
+#ifdef __SCHAR_MAX__
+#define SCHAR_MAX __SCHAR_MAX__
+#else
 #define SCHAR_MAX 127
-#define UCHAR_MAX 0xFF
+#endif
 
-#define INT_MIN -2147483648
-#define INT_MAX 2147483647
-#define UINT_MAX 0xFFFFFFFF
+#define SCHAR_MIN (-SCHAR_MAX -1)
+#define UCHAR_MAX (SCHAR_MAX * 2 + 1)
 
-#define SHRT_MIN -32768
+#ifdef __SHRT_MAX__
+#define SHRT_MAX __SHRT_MAX__
+#else
 #define SHRT_MAX 32767
-#define USHRT_MAX 0xFFFF
+#endif
 
-#define LONG_MIN -9223372036854775808
-#define LONG_MAX 9223372036854775807
-#define ULONG_MAX 0xFFFFFFFFFFFFFFFF
+#define SHRT_MIN (-SHRT_MAX -1)
+#define USHRT_MAX (SHRT_MAX * 2UL + 1)
+
+#ifdef __INT_MAX__
+#define INT_MAX __INT_MAX__
+#else
+#define INT_MAX 2147483647
+#endif
+
+#define INT_MIN (-INT_MAX -1)
+#define UINT_MAX (INT_MAX * 2 + 1)
+
+#ifdef __LONG_MAX__
+#define LONG_MAX __LONG_MAX__
+#elif defined(__i386__)
+#define LONG_MAX INT_MAX
+#else
+#define LONG_MAX 9223372036854775807UL
+#endif
+
+#define LONG_MIN (-LONG_MAX -1)
+#define ULONG_MAX (LONG_MAX * 2UL + 1)
+
+#ifdef __LLONG_MAX__
+#define LLONG_MAX __LLONG_MAX__
+#else
+#define LLONG_MAX LONG_MAX
+#endif
+
+#define LLONG_MIN (-LLONG_MAX -1)
+#define ULLONG_MAX (LLONG_MAX * 2UL + 1)
 
 #define PATH_MAX 256
 

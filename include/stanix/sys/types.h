6782eb3f189009039ec11f5653575542409556fe
tayoky <tayoky4848@gmail.com>
2025-06-01 21:44:25 +0200

    sys/types.h : get size_t and ssize_t from compiler

--- include/stanix/sys/types.h
+++ include/stanix/sys/types.h
@@ -1,36 +1,31 @@
 #ifndef SYS_TYPES_H
 #define SYS_TYPES_H
 
-#ifdef i386
-#define SYS_T int
-#else
-#define SYS_T long
-#endif
 
-typedef SYS_T pid_t;
+typedef long pid_t;
+typedef long uid_t;
+typedef long gid_t;
+typedef long mode_t;
+typedef long nlink_t ;
 
-typedef SYS_T uid_t;
-typedef SYS_T gid_t;
-typedef int mode_t;
-typedef SYS_T nlink_t ;
-
-typedef SYS_T time_t;
-typedef unsigned SYS_T useconds_t;
-typedef SYS_T suseconds_t;
+typedef long time_t;
+typedef unsigned long useconds_t;
+typedef long suseconds_t;
 
 //stupid bloat for posix API
-typedef SYS_T dev_t;
-typedef SYS_T ino_t;
-typedef SYS_T blksize_t; //WTF don't exist and why a program will need that ???
-typedef SYS_T blkcnt_t;
+typedef long dev_t;
+typedef long ino_t;
+typedef long blksize_t;
+typedef long blkcnt_t;
 
-#ifdef i386
-typedef unsigned int off_t;
-typedef unsigned int size_t;
-typedef signed   int ssize_t;
-#else
-typedef unsigned long long off_t;
-typedef unsigned long long size_t;
-typedef signed   long long ssize_t;
+#ifndef __SIZE_TYPE__
+#define __SIZE_TYPE__ unsigned long
 #endif
+#ifndef __PTRDIFF_TYPE__
+#define __PTRDIFF_TYPE__ signed long
+#endif
+
+typedef __SIZE_TYPE__ off_t;
+typedef __SIZE_TYPE__ size_t;
+typedef __PTRDIFF_TYPE__ ssize_t;
 #endif

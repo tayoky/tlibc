c78ea00f9cdbf23e8ae53c49ff72841b68ad02b7
tayoky <tayoky4848@gmail.com>
2025-06-02 19:48:31 +0200

    setjmp : use // comment instead of ;

--- src/x86_64/setjmp.s
+++ src/x86_64/setjmp.s
@@ -3,7 +3,7 @@
 .globl setjmp
 .type setjmp @function
 setjmp:
-	;save callee saved register
+	//save callee saved register
 	movq %rsp, 8(%rdi)
 	movq %rbp, 16(%rdi)
 	movq %r12, 24(%rdi)
@@ -11,11 +11,11 @@ setjmp:
 	movq %r14, 40(%rdi)
 	movq %r15, 48(%rdi)
 
-	;save rip
+	//save rip
 	movq (%rsp), %rax
 	movq %rax, (%rdi)
 
-	;return 0
+	//return 0
 	xor %rax, %rax
 	ret
 .size setjmp, .-setjmp
@@ -23,16 +23,16 @@ setjmp:
 .globl longjmp
 .type longjmp @function
 longjmp:
-	;if rsi is 0 set it to 1
-	cmp %rsi, $0
+	//if rsi is 0 set it to 1
+	cmp $0, %rsi
 	jne skip
 	mov $1, %rsi
 	skip:
 
-	;setup the return value
+	//setup the return value
 	movq %rsi, %rax
 	
-	;restore callee saved register
+	//restore callee saved register
 	movq 8(%rdi) , %rsp
 	movq 16(%rdi), %rbp
 	movq 24(%rdi), %r12
@@ -40,7 +40,7 @@ longjmp:
 	movq 40(%rdi), %r14
 	movq 48(%rdi), %r15
 
-	;return to the saved rip
+	//return to the saved rip
 	mov (%rdi), %rsi
 	jmp *%rsi
 .size longjmp, .-longjmp

6acd681779bfdda99ec9be8c40ca9f3f2351fca1
tayoky <tayoky4848@gmail.com>
2025-06-09 10:00:04 +0200

    math : hard accelerated sqrtf on aarch64 and x86_64

--- math/aarch64/sqrt.s
+++ math/aarch64/sqrt.s
@@ -5,3 +5,10 @@ sqrt:
 	fsqrt d0 ,d0
 	ret
 .size sqrt, .-sqrt
+
+.globl sqrtf
+.type sqrtf @function
+sqrtf:
+	fsqrt s0, s0
+	ret
+.size sqrtf, .-sqrtf
--- math/x86_64/sqrt.s
+++ math/x86_64/sqrt.s
@@ -4,3 +4,11 @@
 sqrt:
 	sqrtsd %xmm0, %xmm0
 	ret
+.size sqrt, .-sqrt
+
+.globl sqrtf
+.type sqrtf @function
+sqrtf:
+	sqrtss %xmm0, %xmm0
+	ret
+.size sqrtf, .-sqrtf

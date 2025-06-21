1b7b728c224195ecf0815f69204c8132ff4a63f8
vizzy-cat <azerytid@gmail.com>
2025-06-21 08:30:54 +0700

    stdint : Add macros and fix intmax

--- include/stdint.h
+++ include/stdint.h
@@ -47,13 +47,8 @@ typedef unsigned int       uint_fast32_t;
 typedef unsigned long long uint_fast64_t;
 
 //max
-#ifdef i386
-typedef int32_t  intmax_t;
-typedef uint32_t uintmax_t;
-#else
 typedef int64_t  intmax_t;
 typedef uint64_t uintmax_t;
-#endif
 
 //ptr
 #ifndef __intptr_t_defined
@@ -61,6 +56,76 @@ typedef uint64_t uintmax_t;
 typedef __PTRDIFF_TYPE__ intptr_t;
 typedef __SIZE_TYPE__    uintptr_t;
 #endif
+
+// integer min limits
+#define INT8_MIN	-128
+#define INT16_MIN	-32768
+#define INT32_MIN	(-2147483647 - 1)
+#define INT64_MIN	(-9223372036854775807LL - 1)
+
+#define INT_LEAST8_MIN	INT8_MIN
+#define INT_LEAST16_MIN	INT16_MIN
+#define INT_LEAST32_MIM INT32_MIN
+#define INT_LEAST64_MIN	INT64_MIN
+
+#define INT_FAST8_MIN	INT8_MIN
+#define INT_FAST16_MIN	INT32_MIN
+#define INT_FAST32_MIN	INT32_MIN
+#define INT_FAST64_MIN	INT64_MIN
+
+#define INTMAX_MIN	INT64_MIN
+#define INTPTR_MIN	INT64_MIN
+
+// integer max limits
+#define INT8_MAX	127
+#define INT16_MAX	32767
+#define INT32_MAX	2147483647
+#define INT64_MAX	9223372036854775807LL
+
+#define INT_LEAST8_MAX	INT8_MAX
+#define INT_LEAST16_MAX	INT16_MAX
+#define INT_LEAST32_MAX	INT32_MAX
+#define INT_LEAST64_MAX	INT64_MAX
+
+#define INT_FAST8_MAX	INT8_MAX
+#define INT_FAST16_MAX	INT32_MAX
+#define INT_FAST32_MAX	INT32_MAX
+#define INT_FAST64_MAX	INT64_MAX
+
+#define INTMAX_MAX	INT64_MAX
+#define INTPTR_MAX	INT64_MAX
+
+#define UINT8_MAX	255
+#define UINT16_MAX	(INT16_MAX * 2 - 1)
+#define UINT32_MAX	(INT32_MAX * 2U - 1)
+#define UINT64_MAX	(INT64_MAX * 2ULL - 1)
+
+#define UINT_LEAST8_MAX	UINT8_MAX
+#define UINT_LEAST16_MAX UINT16_MAX
+#define UINT_LEAST32_MAX UINT32_MAX
+#define UINT_LEAST64_MAX UINT64_MAX
+
+#define UINT_FAST8_MAX	UINT8_MAX
+#define UINT_FAST16_MAX	UINT32_MAX
+#define UINT_FAST32_MAX	UINT32_MAX
+#define UINT_FAST64_MAX	UINT64_MAX
+
+#define UINTMAX_MAX	UINT64_MAX
+#define UINTPTR_MAX	UINT64_MAX
+
+// literal macros
+
+#define INT8_C(x)	x
+#define INT16_C(x)	x
+#define INT32_C(x)	x
+#define INT64_C(x)	x ## LL
+#define INTMAX_C(x)	INT64_C(x)
+
+#define UINT8_C(x)	x
+#define UINT16_C(x)	x
+#define UINT32_C(x)	x ## U
+#define UINT64_C(x)	x ## ULL
+#define UINTMAX_C(x)	UINT64_C(x)
 
 #endif
 #else

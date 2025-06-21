93d4803f14013983593435819884923a4bb096ef
tayoky <tayoky4848@gmail.com>
2025-06-21 16:08:26 +0200

    wchar : single char convertion for up to 0x7ff

--- src/wchar.c
+++ src/wchar.c
@@ -1,10 +1,17 @@
 #include <wchar.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 wint_t btowc(int c){
-	if(c >= 0 && c < 0x80){
+	if(c < 0){
+		return WEOF;
+	} else if(c <= 0x7f){
 		return c;
+	} else if(c <= 0x7ff){
+		const uint8_t b1 = (1<<7) | (c & 0x3f);
+		const uint8_t b2 = (3<<6) | ((c >> 6) & 0x1f);
+		return (b2 << 8) | b1;
 	} else {
 		return WEOF;
 	}

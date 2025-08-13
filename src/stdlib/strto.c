e481e53bd510c3f12894e961fc8e08a2dabf4a3a
tayoky <tayoky4848@gmail.com>
2025-08-13 19:29:08 +0200

    stdlib : fix alpha char in strtoxx

--- src/stdlib/strto.c
+++ src/stdlib/strto.c
@@ -5,8 +5,8 @@
 
 static int char2digit(char c){
 	if(c >= '0' && c <= '9')return c - '0';
-	if(c >= 'A' && c <= 'Z')return c - 'A';
-	if(c >= 'a' && c <= 'z')return c - 'a';
+	if(c >= 'A' && c <= 'Z')return c - 'A' + 10;
+	if(c >= 'a' && c <= 'z')return c - 'a' + 10;
 	return INT_MAX;
 }
 

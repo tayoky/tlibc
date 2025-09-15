0c2a2aa6cdeea05ec03eec245dd7563fe202ff22
tayoky <tayoky4848@gmail.com>
2025-09-15 20:04:09 +0200

    ucontext : make mcontext bigger

--- include/stanix/ucontext.h
+++ include/stanix/ucontext.h
@@ -4,7 +4,7 @@
 #include <sys/signal.h>
 #include <stdint.h>
 
-typedef uintptr_t mcontext_t[32];
+typedef uintptr_t mcontext_t[64];
 typedef struct __ucontext {
     struct __ucontext *uc_link;
     sigset_t    uc_sigmask;

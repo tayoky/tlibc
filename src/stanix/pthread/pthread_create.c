cb4f29c50fb04f8e57ec9094f8036e5dd7118b2f
tayoky <tayoky4848@gmail.com>
2025-10-11 19:35:14 +0200

    pthreads : support for returning from func

--- src/stanix/pthread/pthread_create.c
+++ src/stanix/pthread/pthread_create.c
@@ -1,7 +1,24 @@
 #include <pthread.h>
 #include <sched.h>
 #include <stddef.h>
+#include <stdlib.h>
 
-int pthread_create(pthread_t *thread,const pthread_attr_t *attr,typeof(void *(void *)) *start_routine,void *arg){
-	return clone((void *)start_routine,NULL,CLONE_THREAD,arg,NULL,NULL,&thread->tid);
+struct pthread_args {
+	void *arg;
+	void *(*start_routine)(void *);
+};
+
+static int __pthread_creator(void *arg){
+	struct pthread_args *_args = arg;
+	struct pthread_args args = *_args;
+	free(_args);
+	pthread_exit(args.start_routine(args.arg));
+	return 0;
+}
+
+int pthread_create(pthread_t *thread,const pthread_attr_t *attr,void *(*start_routine)(void *),void *arg){
+	struct pthread_args *args = malloc(sizeof(struct pthread_args));
+	args->arg = arg;
+	args->start_routine = start_routine;
+	return clone((void *)__pthread_creator,NULL,CLONE_THREAD,args,NULL,NULL,&thread->tid);
 }

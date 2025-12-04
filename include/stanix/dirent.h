a9af23dc31ecf545eb522bc62a1b7699e893c7fb
tayoky <tayoky4848@gmail.com>
2025-12-04 18:35:23 +0100

    dirent : add d_type

--- include/stanix/dirent.h
+++ include/stanix/dirent.h
@@ -6,10 +6,20 @@
 struct dirent {
 	char d_name[256]; // pretty sure it should use limtit.h::PATH_MAX
 	ino_t d_ino; //stub inode don't exist in stanix
+	int d_type;
 };
 
 struct _DIR;
 typedef struct _DIR DIR;
+
+#define DT_UNKNOWN  0
+#define DT_REG      1
+#define DT_DIR      2
+#define DT_FIFO     3
+#define DT_SOCK     4
+#define DT_CHR      5
+#define DT_BLK      6
+#define DT_LNK      7
 
 
 //WTF !!!! there are no raw readdir syscall ????

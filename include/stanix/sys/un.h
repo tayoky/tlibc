0a3a46fd79b5aae1975b00a31704345e27786645
tayoky <tayoky4848@gmail.com>
2025-12-08 19:34:29 +0100

    socket : unix socket header

---
+++ include/stanix/sys/un.h
@@ -0,0 +1,11 @@
+#ifndef _SYS_UN_H
+#define _SYS_UN_H
+
+#include <sys/socket.h>
+
+struct sockaddr_un {
+	sa_family_t sun_family;               /* AF_UNIX */
+	char        sun_path[108];            /* Pathname */
+};
+
+#endif

68705d7a0d256692a95b0d6971670e1a86cfc1ba
tayoky <tayoky4848@gmail.com>
2026-01-08 20:04:16 +0100

    stat : fix fstat

--- src/stanix/sys/fstat.c
+++ src/stanix/sys/fstat.c
@@ -3,5 +3,5 @@
 #include <errno.h>
 
 int fstat(int fd,struct stat *st){
-	return __set_errno(__syscall2(SYS_stat,(long)fd,(long)st));
+	return __set_errno(__syscall2(SYS_fstat,(long)fd,(long)st));
 }

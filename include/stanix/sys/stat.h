041c1394d633e9480d238f1e8714ae91a2a9113a
tayoky <tayoky4848@gmail.com>
2026-02-13 19:52:20 +0100

    stat : posix compliant numbers

--- include/stanix/sys/stat.h
+++ include/stanix/sys/stat.h
@@ -5,19 +5,19 @@
 #include <sys/types.h>
 
 struct stat {
-	dev_t     st_dev;      //ID of device containing file
-	ino_t     st_ino;      //file serial number
-	mode_t    st_mode;     //mode of file (see below)
-	nlink_t   st_nlink;    //number of links to the file
-	uid_t     st_uid;      //user ID of file
-	gid_t     st_gid;      //group ID of file
-	dev_t     st_rdev;     //device ID (if file is character or block special)
-	off_t     st_size;     //file size in bytes (if file is a regular file)
-	time_t    st_atime;    //time of last access
-	time_t    st_mtime;    //time of last data modification
-	time_t    st_ctime;    //time of last status change
-	blksize_t st_blksize;  //a filesystem-specific preferred I/O block size for
-	blkcnt_t  st_blocks;   //number of blocks allocated for this object
+	dev_t     st_dev;      // ID of device containing file
+	ino_t     st_ino;      // file serial number
+	mode_t    st_mode;     // mode of file (see below)
+	nlink_t   st_nlink;    // number of links to the file
+	uid_t     st_uid;      // user ID of file
+	gid_t     st_gid;      // group ID of file
+	dev_t     st_rdev;     // device ID (if file is character or block special)
+	off_t     st_size;     // file size in bytes (if file is a regular file)
+	time_t    st_atime;    // time of last access
+	time_t    st_mtime;    // time of last data modification
+	time_t    st_ctime;    // time of last status change
+	blksize_t st_blksize;  // a filesystem-specific preferred I/O block size for
+	blkcnt_t  st_blocks;   // number of blocks allocated for this object
 };
 
 #define S_IRUSR 0400
@@ -35,22 +35,21 @@ struct stat {
 #define S_IXOTH 01
 #define S_IRWXO S_IROTH | S_IWOTH | S_IXOTH
 
-#define S_ISVTX 01000 //restriction deletion bit
-#define S_ISUID 02000 //set uid bit
-#define S_ISGID 04000 //set gid bit
+#define S_ISUID 04000 // set uid bit
+#define S_ISGID 02000 // set gid bit
+#define S_ISVTX 01000 // restriction deletion bit
 
-//files types
-#define S_IFBLK  00010000 //block device
-#define S_IFCHR  00020000 //character device
-#define S_IFIFO  00040000 //FIFO (Firt In First Out) 
-#define S_IFREG  00100000 //regular file
-#define S_IFDIR  00200000 //directory
-#define S_IFLNK  00400000 //symbolic link
-#define S_IFSOCK 01000000 //socket
+// files types
+#define S_IFMT   0170000
+#define S_IFSOCK 0140000 // socket
+#define S_IFLNK  0120000 // symbolic link
+#define S_IFREG  0100000 // regular file
+#define S_IFBLK  0060000 // block device
+#define S_IFDIR  0040000 // directory
+#define S_IFCHR  0020000 // character device
+#define S_IFIFO  0010000 // FIFO (Firt In First Out) 
 
-#define S_IFMT   01770000
-
-//types test
+// types test
 
 #define S_ISBLK(m)  ((m & S_IFMT) == S_IFBLK) //Test for a block special file. 
 #define S_ISCHR(m)  ((m & S_IFMT) == S_IFCHR) //Test for a character special file. 
@@ -60,7 +59,7 @@ struct stat {
 #define S_ISLNK(m)  ((m & S_IFMT) == S_IFLNK) //Test for a symbolic link. 
 #define S_ISSOCK(m) ((m & S_IFMT) == S_IFSOCK) //Test for a socket. 
 
-//functions
+// functions
 int stat(const char *pathname,struct stat *st);
 int fstat(int fd,struct stat *st);
 int lstat(const char *pathname,struct stat *st);

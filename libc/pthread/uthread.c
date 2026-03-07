e0f9f7d0ba99769afb182912abbff94172fe1a08
tayoky <tayoky4848@gmail.com>
2026-03-07 19:37:27 +0100

    linker : do not mmap uthread

--- libc/pthread/uthread.c
+++ libc/pthread/uthread.c
@@ -5,6 +5,17 @@
 
 #define UTHREAD_SIZE ((sizeof(struct __uthread) + PAGE_SIZE - 1) / PAGE_SIZE * PAGE_SIZE)
 
+#ifdef __LD_TLIBC__
+struct __uthread *__new_uthread(void){
+    // dynamic linker cannot call mmap
+    static struct __uthread uthread;
+
+    uthread.self = &uthread;
+    uthread.err = 0;
+
+    return &uthread;
+}
+#else
 struct __uthread *__new_uthread(void){
     struct __uthread *uthread = mmap(NULL,UTHREAD_SIZE,PROT_READ | PROT_WRITE,MAP_PRIVATE | MAP_ANONYMOUS,0,0);
     if(uthread == MAP_FAILED){
@@ -16,6 +27,7 @@ struct __uthread *__new_uthread(void){
 
     return uthread;
 }
+#endif
 
 void __free_uthread(struct __uthread *uthread){
     munmap(uthread,UTHREAD_SIZE);

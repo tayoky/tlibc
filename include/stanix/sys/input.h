cdbf5d9af52c6c4c14aa8e50fdbc67650294e63a
tayoky <tayoky4848@gmail.com>
2026-03-23 19:12:54 +0100

    input.h : add more keycodes

--- include/stanix/sys/input.h
+++ include/stanix/sys/input.h
@@ -15,8 +15,7 @@ struct input_event {
             long y;
         } ie_move;
         struct {
-            char c;
-            char padding[3];
+            unsigned long key;
             unsigned long scancode;
             unsigned long flags;
         } ie_key;
@@ -86,8 +85,52 @@ struct input_request {
 
 #define INPUT_LAYOUT_SIZE 64
 
-#define INPUT_KEY_MOUSE_LEFT   1024
-#define INPUT_KEY_MOUSE_MIDDLE 1025
-#define INPUT_KEY_MOUSE_RIGHT  1026
+#define INPUT_KEY_FIRST        17000000
+#define INPUT_KEY_MOUSE_LEFT   17000000
+#define INPUT_KEY_MOUSE_MIDDLE 17000001
+#define INPUT_KEY_MOUSE_RIGHT  17000002
+#define INPUT_KEY_ESC          17000051
+#define INPUT_KEY_TAB          17000052
+#define INPUT_KEY_BACKSPACE    17000053
+#define INPUT_KEY_ENTER        17000054
+#define INPUT_KEY_DELETE       17000055
+#define INPUT_KEY_INERT        17000056
+#define INPUT_KEY_HOME         17000057
+#define INPUT_KEY_END          17000058
+#define INPUT_KEY_ARROW_UP     17000059
+#define INPUT_KEY_ARROW_DOWN   17000060
+#define INPUT_KEY_ARROW_DOWN   17000061
+#define INPUT_KEY_ARROW_LEFT   17000062
+#define INPUT_KEY_ARROW_RIGHT  17000063
+#define INPUT_KEY_PAGE_UP      17000064
+#define INPUT_KEY_PAGE_DOWN    17000065
+#define INPUT_KEY_LSHIFT       17000066
+#define INPUT_KEY_RSHIFT       17000067
+#define INPUT_KEY_LCRTL        17000068
+#define INPUT_KEY_RCRTL        17000069
+#define INPUT_KEY_LALT         17000070
+#define INPUT_KEY_RALT         17000071
+#define INPUT_KEY_ALTGR        17000072
+#define INPUT_KEY_NUM_LOCK     17000073
+#define INPUT_KEY_SCROLL_LOCK  17000074
+#define INPUT_KEY_CAPS_LOCK    17000075
+#define INPUT_KEY_F1           17000076
+#define INPUT_KEY_F2           17000077
+#define INPUT_KEY_F3           17000078
+#define INPUT_KEY_F4           17000079
+#define INPUT_KEY_F5           17000080
+#define INPUT_KEY_F6           17000081
+#define INPUT_KEY_F7           17000082
+#define INPUT_KEY_F8           17000083
+#define INPUT_KEY_F9           17000084
+#define INPUT_KEY_F10          17000085
+#define INPUT_KEY_F11          17000086
+#define INPUT_KEY_F12          17000087
+#define INPUT_KEY_GUI          17000088
+#define INPUT_KEY_VOLUME_UP    17000089
+#define INPUT_KEY_VOLUME_DOWN  17000090
+#define INPUT_KEY_LAST         17000090
+
+#define INPUT_KEY_SPACE ' '
 
 #endif

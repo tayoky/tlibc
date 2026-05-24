bb34a2f5e83f5b7051b54274b09cf4627ed504f5
tayoky <tayoky4848@gmail.com>
2026-05-24 15:43:21 +0200

    libm : asin : real asin implementation

--- libm/generic/asin.c
+++ libm/generic/asin.c
@@ -1,14 +1,55 @@
 #include <math.h>
+#include <errno.h>
 #include "internal.h"
 
-float asinf(float x) {
-	// TODO : actual asinf impl
-	(void)x;
-	return 0;
+static const double P[] = {
+	 0.16666666666666666,
+	-0.01249999999757921,
+};
+
+static const double Q[] = {
+	 1.0,
+	-0.2142857141014798,
+};
+
+// i found this online
+static double asin_core(double x) {
+	double x2 = x * x;
+	double x3 = x2 * x;
+
+	// evaluate the rational polynomial P(x^2) / Q(x^2) via Horner's Method
+	double num = P[0] + x2 * P[1];
+	double den = Q[0] + x2 * Q[1];
+
+	return x + (x3 * (num / den));
 }
 
 double asin(double x) {
-	// TODO : actual asin impl
-	(void)x;
-	return 0;
+	if (isnan(x)) return x;
+
+	if (x > 1.0 || x < -1.0) {
+		errno = EDOM;
+		return NAN;
+	}
+
+	double sign = 1.0;
+	if (x < 0) {
+		x = -x;
+		sign = -1.0;
+	}
+
+	// fast path
+	if (x == 0) return 0.0;
+	if (x == 1.0) return sign * M_PI_2;
+
+	if (x <= 0.5) {
+		// central zone
+		return sign * asin_core(x);
+	} else {
+		// edge zone
+		double t = sqrt(0.5 * (1.0 - x));
+
+		// scale the result back
+		return sign * (M_PI_2 - 2.0 * asin_core(t));
+	}
 }
---
+++ libm/generic/asinf.c
@@ -0,0 +1,55 @@
+#include <math.h>
+#include <errno.h>
+#include "internal.h"
+
+static const float P[] = {
+	 0.16666666666666666f,
+	-0.01249999999757921f,
+};
+
+static const float Q[] = {
+	 1.0f,
+	-0.2142857141014798f,
+};
+
+// i found this online
+static float asin_core(float x) {
+	float x2 = x * x;
+	float x3 = x2 * x;
+
+	// evaluate the rational polynomial P(x^2) / Q(x^2) via Horner's Method
+	float num = P[0] + x2 * P[1];
+	float den = Q[0] + x2 * Q[1];
+
+	return x + (x3 * (num / den));
+}
+
+float asinf(float x) {
+	if (isnan(x)) return x;
+
+	if (x > 1.0f || x < -1.0f) {
+		errno = EDOM;
+		return NAN;
+	}
+
+	float sign = 1.0f;
+	if (x < 0) {
+		x = -x;
+		sign = -1.0f;
+	}
+
+	// fast path
+	if (x == 0) return 0.0f;
+	if (x == 1.0f) return sign * M_PI_2;
+
+	if (x <= 0.5f) {
+		// central zone
+		return sign * asin_core(x);
+	} else {
+		// edge zone
+		float t = sqrtf(0.5f * (1.0f - x));
+
+		// scale the result back
+		return sign * (M_PI_2 - 2.0f * asin_core(t));
+	}
+}

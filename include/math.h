516adac3c432947e0396261459a7f6dafa341635
tayoky <tayoky4848@gmail.com>
2026-06-21 18:13:38 +0200

    lib : add more constants

--- include/math.h
+++ include/math.h
@@ -113,13 +113,22 @@ ffunc(exp)
 #undef ffunc
 #undef ffunc2
 
-//constant
+// constants
 
-#define M_PI 3.1415926535f
-#define M_PI_2 (M_PI * 0.5f)
-#define M_PI_4 (M_PI * 0.25f)
-#define M_1_PI (1.0f / M_PI)
-#define M_2_PI (2.0f / M_PI)
+#define M_E             2.7182818284590452354
+#define M_LOG2E         1.4426950408889634074
+#define M_LOG10E        0.43429448190325182765
+#define M_LN2           0.69314718055994530942
+#define M_LN10          2.30258509299404568402
+#define M_PI            3.14159265358979323846
+#define M_PI_2          1.57079632679489661923
+#define M_PI_4          0.78539816339744830962
+#define M_1_PI          0.31830988618379067154
+#define M_2_PI          0.63661977236758134308
+#define M_2_SQRTPI      1.12837916709551257390
+#define M_SQRT2         1.41421356237309504880
+#define M_SQRT1_2       0.70710678118654752440
+
 #ifndef NAN
 #define NAN __builtin_nanf("")
 #endif

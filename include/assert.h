6e5f7a242fa4c3fd533fd5209db62294c93ad476
tayoky <tayoky4848@gmail.com>
2026-06-21 14:17:21 +0200

    assert : add static_assert

--- include/assert.h
+++ include/assert.h
@@ -3,6 +3,14 @@
 
 void __assert(const char *expr,const char *file,long line,const char *func);
 
+#if !defined(__cplusplus) && __STDC_VERSION__ < 202311L
+#if __STDC_VERSION__ >= 201112L
+#define static_assert _Static_assert
+#else
+#define static_assert(cond, msg) static char _static_assert_ ## __LINE__[((cond) && (msg)) - 1];
+#endif
+#endif
+
 #endif
 
 #undef assert

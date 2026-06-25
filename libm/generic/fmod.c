9d1c4ee03b2c7cf65f5bdff7729f258e2f708e5f
tayoky <tayoky4848@gmail.com>
2026-06-25 19:05:58 +0200

    libm : improve fmod

--- libm/generic/fmod.c
+++ libm/generic/fmod.c
@@ -4,6 +4,8 @@
 double fmod(double x, double y) {
 	if (isnan(x) || isnan(y) || isinf(x) || y == 0) return NAN;
 	if (isinf(y)) return x;
-	double quotien = trunc(x / y);
-	return x - quotien * y;
+    double q = x / y;
+    double n = (q > 0) ? floor(q) : ceil(q);
+
+    return x - n * y;
 }

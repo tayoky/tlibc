3a10d60f56b8585ab346870cde6d09583de8e99d
tayoky <tayoky4848@gmail.com>
2026-08-12 17:52:13 +0200

    pthread and signal : fix two bugs

--- libc/pthread/pthread_sigqueue.c
+++ libc/pthread/pthread_sigqueue.c
@@ -1,11 +1,12 @@
 #include <pthread.h>
 #include <signal.h>
+#include <sysdeps.h>
 #include <unistd.h>
 #include <tlibc.h>
 #include <errno.h>
 
 int pthread_sigqueue(pthread_t thread, int sig, const union sigval value) {
-	int ret = tgsigqueue(getpid(), thread->tid, sig, value);
+	int ret = sys_tgsigqueue(getpid(), thread->tid, sig, value);
 	if (ret < 0) return errno;
 	return 0;
 }
--- libc/signal/tgkill.c
+++ libc/signal/tgkill.c
@@ -2,5 +2,5 @@
 #include <signal.h>
 
 int tgkill(pid_t pid, pid_t tid, int sig) {
-	return sys_tkgill(pid, tid, sig);
+	return sys_tgkill(pid, tid, sig);
 }

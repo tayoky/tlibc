f2a76e341c0fe36bc5a5fa4834941a7420b964cb
tayoky <tayoky4848@gmail.com>
2026-08-16 00:33:47 +0200

    generic : fix sys_rmdir

--- sysdeps/generic/unistd/sys_rmdir.c
+++ sysdeps/generic/unistd/sys_rmdir.c
@@ -7,10 +7,10 @@ TLIBC_WEAK int sys_rmdir(const char *pathname) {
 	// try to emulate rmdir using unlink
 	// might require root
 	char buf[PATH_MAX];
-	snprintf(buf, sizeof(buf), "%s/.");
+	snprintf(buf, sizeof(buf), "%s/.", pathname);
 	int ret = unlink(buf);
 	if (ret < 0) return ret;
-	snprintf(buf, sizeof(buf), "%s/..");
+	snprintf(buf, sizeof(buf), "%s/..", pathname);
 	ret = unlink(buf);
 	if (ret < 0) return ret;
 	return unlink(pathname);

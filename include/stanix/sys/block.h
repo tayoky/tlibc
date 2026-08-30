8b3d6c15c9bdc08616545112ed715cfd7699133e
tayoky <tayoky4848@gmail.com>
2026-08-30 14:51:42 +0200

    stanix : update sys/block.h again

--- include/stanix/sys/block.h
+++ include/stanix/sys/block.h
@@ -3,15 +3,27 @@
 
 #include <sys/types.h>
 
-// deprecated, use BLOCK_GET_TOPOLOGY instead
+// deprecated, use BLOCK_GET_DISK_INFO instead
 #define BLOCK_GET_SIZE     17000
 
-#define BLOCK_GET_TOPOLOGY 17001
-#define BLOCK_RESCAN_PARTS 17002
+#define BLOCK_GET_DISK_INFO 17001
+#define BLOCK_RESCAN_PARTS  17002
+#define BLOCK_GET_PART_INFO 17003
+#define BLOCK_OPEN_DISK     17004
 
-typedef struct block_topology {
+typedef struct block_disk_info {
+	char uuid[64];
+	char partition_table_type[64];
 	size_t logical_block_size;
 	size_t blocks_count;
-} block_topology_t;
+	char reserved[128];
+} block_disk_info_t;
+
+typedef struct block_part_info {
+	char uuid[64];
+	off_t offset;
+	size_t size;
+	char reserved[128];
+} block_part_info_t;
 
 #endif

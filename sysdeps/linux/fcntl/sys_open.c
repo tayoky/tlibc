27df372a7b5a2b21594506fb689af5b8daea7944
tayoky <tayoky4848@gmail.com>
2026-09-06 16:05:45 +0200

    linux : fix sys_open

--- sysdeps/linux/fcntl/sys_open.c
+++ sysdeps/linux/fcntl/sys_open.c
@@ -1,9 +1,14 @@
 #include <sysdeps.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #ifdef SYS_open
 int sys_open(const char *pathname, int flags, mode_t mode) {
 	return syscall(SYS_open, pathname, flags, mode);
+}
+#else
+int sys_open(const char *pathname, int flags, mode_t mode) {
+	return sys_openat(AT_FDCWD, pathname, flags, mode);
 }
 #endif

f1a25aa9ce435ee33c4219b2659efef5dc813bc0
tayoky <tayoky4848@gmail.com>
2026-09-23 19:20:13 +0200

    locale : fix setlocale

--- libc/locale/setlocale.c
+++ libc/locale/setlocale.c
@@ -5,13 +5,22 @@
 extern struct __locale _locale;
 
 char *setlocale(int category, const char *locale) {
-	if (category > LC_ALL) return NULL;
+	if (category > LC_ALL || category < 0) return NULL;
+
+	if (locale) {
+		locale_t new_locale = NULL;
+		if (category == LC_ALL) {
+			new_locale = newlocale(LC_ALL_MASK, locale, &_locale)->locales[0]->name;
+		} else {
+			new_locale = newlocale(1 << category, locale, &_locale)->locales[category]->name;
+		}
+		if (!new_locale) return NULL;
+		_locale = *new_locale;
+	}
 
 	if (category == LC_ALL) {
-		if (!locale) return _locale.locales[0]->name;
-		return newlocale(LC_ALL_MASK, locale, &_locale)->locales[0]->name;
+		return _locale.locales[0]->name;
 	} else {
-		if (!locale) return _locale.locales[category]->name;
-		return newlocale(1 << category, locale, &_locale)->locales[category]->name;
+		return _locale.locales[category]->name;
 	}
 }

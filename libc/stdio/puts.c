c3c16e4e925145d8add873fb0d6e3d9661711a94
tayoky <tayoky4848@gmail.com>
2026-09-26 23:14:17 +0200

    stdio : make puts atomic

--- libc/stdio/puts.c
+++ libc/stdio/puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int puts(const char *str) {
+	flockfile(stdout);
 	if (fputs(str, stdout) < 0) {
+		funlockfile(stdout);
 		return -1;
 	}
-	return putchar('\n') != EOF ? 0 : -1;
+	int ret = putchar_unlocked('\n') != EOF ? 0 : -1;
+	funlockfile(stdout);
+	return ret;
 }
